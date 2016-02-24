2e5b9c8fc5c609ef19905a98728ceffe6ad67d55
triktron <ele.trik@hotmail.com>
2016-02-24 19:28:45 +0100

    restart

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + "/client"));
-
-app.listen(port);
-console.log("listening on *:" + port);
